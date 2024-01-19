3ab50941688f282d4b216b8e408b4c87072ccee9
Eberson Oliveira <eberson.olivera@gmail.com>
2024-01-18 23:12:07 -0300

    created local environment

--- infra/docker/mysql/script/setup.sql
+++ infra/docker/mysql/script/setup.sql
@@ -1,7 +1,7 @@
-create database if not exists tech_challenge character set utf8mb4 collate utf8mb4_0900_ai_ci;
+create database if not exists purchase character set utf8mb4 collate utf8mb4_0900_ai_ci;
 
-create user if not exists 'sys_tech_challenge'@'%' identified with mysql_native_password by '6#233zJMNTjkQb#sgps#';
+create user if not exists 'sys_purchase'@'%' identified with mysql_native_password by 'purchase_6#233zJMNTjkQb#sgps#';
 
-grant all privileges on tech_challenge.* to 'sys_tech_challenge'@'%';
+grant all privileges on purchase.* to 'sys_purchase'@'%';
 
 flush privileges;
