10dc2cea752fab2729a03744c93ccf60cfb7aad8
Eberson Oliveira <eberson.olivera@gmail.com>
2024-02-19 16:10:38 -0300

    feat: created module to deal with purchase events received from cart

--- drivers/cart-closed-consumer/src/main/java/br/com/fiap/tech/challenge/purchase/driver/cart/closed/consumer/config/CartClosedConsumerConfiguration.java
+++ drivers/cart-closed-consumer/src/main/java/br/com/fiap/tech/challenge/purchase/driver/cart/closed/consumer/config/CartClosedConsumerConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "br.com.fiap.tech.challenge.purchase.driver.payment.consumer")
+@ComponentScan(basePackages = "br.com.fiap.tech.challenge.purchase.driver.cart.closed.consumer")
 public class CartClosedConsumerConfiguration {
 
 }
